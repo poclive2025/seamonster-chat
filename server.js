5cf9eabac3ffc2c53927b9bd99348355ca434dd3
poclive2025 <poclive2025@gmail.com>
2025-05-15 15:12:06 +0000

    cors delete

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-
-
 
 const express = require('express');
 const path = require('path');
