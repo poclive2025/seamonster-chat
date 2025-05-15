1962d734491e2b056af6b9c03e8848720922bced
poclive2025 <poclive2025@gmail.com>
2025-05-15 18:36:23 +0000

    changes 22

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+
+
+
+
+
+const express = require('express');
+
 const cors = require("cors");
+
+
+const path = require('path');
+const app = express();
 
 app.use(cors());
 
 
-const express = require('express');
-const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
